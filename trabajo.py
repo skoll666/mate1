1b444f844bf126da8b90b5bd13c4573a433add30
skoll666 <153628772+skoll666@users.noreply.github.com>
2023-12-12 09:47:59 -0500

    Add files via upload

---
+++ trabajo.py
@@ -0,0 +1,27 @@
+#El propósito general de este programa es realizar Operaciones Básicas y Tipos de Datos, Manipulación de Cadenas de Texto y Conversión de Tipos de Datos.
+#Ejercicio 1: Operaciones Básicas y Tipos de Datos
+a = 30+20 #En esta variable se puede realizar la suma de números enteros.
+b = 2.4-1.2 #En esta variable se puede realizar la resta de números flotantes.
+c = 5*5 #En esta variable se puede realizar la multiplicación de números enteros.
+d = 16.8/4.2 #En esta variable se puede realizar la división de números flotantes.
+
+print ("el resultado de la suma entre 30+20 es:", a)
+print ("el resultado de la resta entre 2.4-1.2 es:", b)
+print ("el resultado de la multiplicación entre 5*5 es:", c)
+print ("el resultado de la división entre 16.8/4.2 es:", d)
+#El propósito de este ejercicio u programa es realizar suma, resta, multiplicación y división, ya sea de numeros enteros o flotantes.
+
+#Ejercicio 2: Manipulación de Cadenas de Texto
+e = "Cristopher Ayrton Galarza Ramirez" #En esta variable se coloca mi nombre.
+f = len (e) #Se coloca la variable len() para obtener la longitud del nombre colocado en la anterior variable.
+print (e,f)
+#El propósito de este ejercicio u programa es contar los caracteres que se presentan en una variable, en este caso, la variable es mi nombre.
+
+#Ejercicio 3: Conversión de Tipos de Datos
+g = 7 #En esta variable se coloca una variable con un número entero.
+h = 2.5 #En esta variable se coloca una variable con un número flotante.
+i = float (g) #En la variable float() se utiliza para convertir el número entero a uno flotante.
+j = int (h) #En la variable int() se utiliza para convertir el número flotante a uno entero.
+print (g,i)
+print (h,j)
+#El propósito de este ejercicio u programa es transformas un número entero a flotante y uno flotante a entero.
